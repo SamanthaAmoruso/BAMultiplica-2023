e948e450be8ea3c649d0a4009350734b8c2087b7
= <=>
2023-04-27 22:11:57 -0300

    probando doble punto

--- scripts/shows.js
+++ scripts/shows.js
@@ -49,7 +49,7 @@ for (let i = 0; i < infoDivs.length; i++)
   orden.classList.add('orden');
 
   const imagen = document.createElement('img');
-  imagen.src = '/images/download.png';
+  imagen.src = '../images/download.png';
   imagen.alt = 'velez';
   imagen.classList.add('contenedorCircular');
 
